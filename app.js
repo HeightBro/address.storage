d2f378b2c7402d102a0d30ac1881554dd3885380
Oleg Frolov <olegfunictech@gmail.com>
2021-12-03 00:42:31 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// Dependencies
+const express = require("express");
+const http = require("http");
+
+// Server
+const app = express();
+const server = http.createServer(app);
+const port = normalizePort(process.env.PORT || "3000");
+
+// Routers
+const telegramRouter = require("./src/routes/telegram");
+
+app.set("port", port);
+
+app.use(express.json());
+app.use("/", telegramRouter);
+
+server.listen(port);
+server.on("listening", onListening);
+server.on("error", onError);
+
+/**
+ * Нормализовать порт в число, строку или false
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ?
+    "pipe " + addr :
+    "port " + addr.port;
+
+  console.log(`Server has been started on ${bind}`);
+}
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ?
+    "Pipe " + port :
+    "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
---
+++ src/routes/telegram.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+// Check api is available
+router.get("/", (req, res) => {
+    return res.status(200).send({ status: "Api is available" });
+});
+
+// webhook
+router.post("/", (req, res) => {
+    /*
+        You can put the logic you want here
+        the message receive will be in this
+        https://core.telegram.org/bots/api#update
+      */
+    const isTelegramMessage = req.body &&
+        req.body.message &&
+        req.body.message.chat &&
+        req.body.message.chat.id &&
+        req.body.message.from &&
+        req.body.message.from.firstName;
+
+    if (isTelegramMessage) {
+        const chatId = req.body.message.chat.id;
+        const { firstName } = req.body.message.from;
+
+        return res.status(200).send({
+            method: "sendMessage",
+            chatId,
+            text: `Привет, ${firstName}`,
+        });
+    }
+
+    return res.status(200).send({ status: "not a telegram message" });
+});
